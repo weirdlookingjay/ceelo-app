cf6bc5b5f76cd0f79a69959abec39b0695cdcfd0
weirdlookingjay <weirdlookingjay@live.com>
2018-09-05 15:50:09 -0400

    Added Simple Tests

---
+++ README.md
@@ -0,0 +1,1 @@
+## A simple Ceelo Flask App
--- ceelo/app.py
+++ ceelo/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from ceelo.blueprints.page import page
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a Flask application using the app factory pattern.
 
@@ -11,6 +11,9 @@ def create_app():
 
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
 
     app.register_blueprint(page)
 
